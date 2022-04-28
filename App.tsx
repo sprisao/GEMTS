a4cc383f7f8931313813c563b266990c7986a670
르네상스맨 <whdtmek@gmail.com>
2022-04-29 07:39:05 +0900

    [Edit] App.tsx 에서 로그인한 유저의 상태 업데이트 체크하는 코드 제거(일단 필요시 각 컴포넌트에서 사용)

--- App.tsx
+++ App.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {useEffect, useState} from 'react';
-import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+// import {useEffect, useState} from 'react';
+// import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import StackNavigation from './navigation/StackNavigation';
 
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
+// import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 const App = () => {
-  const [initializing, setInitializing] = useState<boolean>(true);
-  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  // const [initializing, setInitializing] = useState<boolean>(true);
+  // const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
 
-  const googleSignin = () => {
-    GoogleSignin.configure({
-      webClientId:
-        '592721340078-98ogm20c8hhr2tkev4ppjj6agk3lcahj.apps.googleusercontent.co',
-    });
-  };
+  // const googleSignin = () => {
+  //   GoogleSignin.configure({
+  //     webClientId:
+  //       '592721340078-98ogm20c8hhr2tkev4ppjj6agk3lcahj.apps.googleusercontent.co',
+  //   });
+  // };
 
-  useEffect(() => {
-    googleSignin();
-  });
+  // function onAuthStateChanged(user) {
+  //   setUser(user);
+  //   if (initializing) {
+  //     setInitializing(false);
+  //   }
+  // }
 
-  useEffect(() => {
-    auth().onAuthStateChanged(userState => {
-      setUser(userState);
-      if (initializing) {
-        setInitializing(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   googleSignin();
+  // });
+
+  // useEffect(() => {
+  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //   return subscriber;
+  // }, []);
 
   return (
     <NavigationContainer>
