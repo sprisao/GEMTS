bca333a40bcd25fde7a50e749e017bc842fe7c45
르네상스맨 <whdtmek@gmail.com>
2022-05-02 19:35:54 +0900

    [Navigation] ChatStack 추가

---
+++ src/screens/Chat/ChatLobby.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ChatLobby = (props: Props) => {
+  return (
+    <View>
+      <Text>ChatMain</Text>
+    </View>
+  );
+};
+
+export default ChatLobby;
