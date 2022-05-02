a637959d6cc23a8eac2299f612b8f21b12235272
르네상스맨 <whdtmek@gmail.com>
2022-05-02 13:56:13 +0900

    [Navigation] StackNavigation은 출발점(Tab)이 다를 뿐 모든 스크린을 타도록 설정

--- navigation/HomeStackNavigation.tsx
+++ navigation/HomeStackNavigation.tsx
@@ -3,6 +3,10 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from '../src/screens/HomeScreen/HomeScreen';
+import LoginScreen from '../src/screens/LoginScreen/LoginScreen';
+import RegisterScreen from '../src/screens/RegisterScreen/RegisterScreen';
+import PwResetScreen from '../src/screens/PwResetScreen/PwResetScreen';
+import ProfileScreen from '../src/screens/ProfileScreen/ProfileScreen';
 import StoreDisplayScreen from '../src/screens/StoreDisplayScreen/StoreDisplayScreen';
 import StoreDetailScreen from '../src/screens/StoreDetailScreen/StoreDetailScreen';
 import ServiceDisplayScreen from '../src/screens/ServiceDisplayScreen/ServiceDisplayScreen';
@@ -20,39 +24,51 @@ type RootStackParamList = {
   StoreDetail: undefined;
   ServiceDisplay: undefined;
   ServiceDetail: undefined;
+  //   Profile: {userId: string};
+  //   Feed: {sort: 'latest' | 'top'} | undefined;
 };
 
-const HomeStack = createNativeStackNavigator<RootStackParamList>();
+// type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function StackNavigation() {
   return (
-    <HomeStack.Navigator initialRouteName="Home">
-      <HomeStack.Screen name="Home" component={HomeScreen} options={{}} />
-      <HomeStack.Screen
+    <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Screen name="Home" component={HomeScreen} options={{}} />
+      <RootStack.Screen name="Login" component={LoginScreen} options={{}} />
+      <RootStack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{}}
+      />
+      <RootStack.Screen name="PwReset" component={PwResetScreen} options={{}} />
+      <RootStack.Screen
         name="StoreDisplay"
         component={StoreDisplayScreen}
         options={{}}
       />
-      <HomeStack.Screen
+      <RootStack.Screen
         name="StoreDetail"
         component={StoreDetailScreen}
         options={{}}
       />
-      <HomeStack.Screen
+      <RootStack.Screen
         name="ServiceDisplay"
         component={ServiceDisplayScreen}
         options={{}}
       />
-      <HomeStack.Screen
+      <RootStack.Screen
         name="ServiceDetail"
         component={ServiceDetailScreen}
         options={{}}
       />
-      <HomeStack.Screen
+      <RootStack.Screen name="Profile" component={ProfileScreen} options={{}} />
+      <RootStack.Screen
         name="CurationDisplay"
         component={CurationDisplayScreen}
         options={{}}
       />
-    </HomeStack.Navigator>
+    </RootStack.Navigator>
   );
 }
