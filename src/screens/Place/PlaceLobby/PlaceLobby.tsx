a8793f8c7042b903dcaee56aee5db4bf26e7df3a
르네상스맨 <whdtmek@gmail.com>
2022-05-05 21:38:16 +0900

    [PlaceLobby] 리팩터링 : Styled-Component 활용하여 최적화

--- src/screens/Place/PlaceLobby/PlaceLobby.tsx
+++ src/screens/Place/PlaceLobby/PlaceLobby.tsx
@@ -1,5 +1,3 @@
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {View} from 'react-native';
 import React from 'react';
 
 import {ThemeProvider} from 'styled-components';
@@ -11,42 +9,47 @@ import {FirstCategoryEmojis} from '../../../constants/Assets';
 import PlaceLobbyHeader from './components/PlaceLobbyHeader';
 import {ButtonLarge} from './components/PlaceLobbyButtons';
 
-const ContentScreen = styled.ScrollView`
+const SafeAreaView = styled.SafeAreaView`
   flex: 1;
+`;
+
+const ScrollView = styled.ScrollView`
+  flex: 1;
+`;
+
+const ButtonWrapper = styled.View`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin-top: 3px;
+  padding-top: 15px;
 `;
 
 interface Props {}
 const PlaceLobby = ({}: Props) => {
   return (
     <ThemeProvider theme={Theme}>
-      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
-        <View style={{flex: 1}}>
-          <PlaceLobbyHeader />
-          <ContentScreen>
-            <View
-              style={{
-                width: '100%',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                paddingHorizontal: 10,
-                marginTop: 3,
-                paddingTop: 15,
-              }}>
-              <ButtonLarge
-                name={'맛집'}
-                message={'검색없이 한번에\n바로찾는 원주맛집'}
-                id={'restaurant'}
-                emoji={FirstCategoryEmojis.restaurant}
-              />
-              <ButtonLarge
-                name={'카페'}
-                message={'원주의\n모든 카페를 한 눈에!'}
-                id={'cafe'}
-                emoji={FirstCategoryEmojis.cafe}
-              />
-            </View>
-          </ContentScreen>
-        </View>
+      <SafeAreaView>
+        <PlaceLobbyHeader />
+        <ScrollView>
+          <ButtonWrapper>
+            <ButtonLarge
+              name={'맛집'}
+              message={'검색없이 한번에\n바로찾는 원주맛집'}
+              id={'restaurant'}
+              emoji={FirstCategoryEmojis.restaurant}
+            />
+            <ButtonLarge
+              name={'카페'}
+              message={'원주의\n모든 카페를 한 눈에!'}
+              id={'cafe'}
+              emoji={FirstCategoryEmojis.cafe}
+            />
+          </ButtonWrapper>
+        </ScrollView>
       </SafeAreaView>
     </ThemeProvider>
   );
