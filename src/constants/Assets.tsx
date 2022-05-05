ae93de5873bfe32f553b43f609100ab0a676ccfd
르네상스맨 <whdtmek@gmail.com>
2022-05-05 19:38:37 +0900

    [Add] Constant 파일 위치 및 파일명 변경 (/src/constants/Assets.tsx)

---
+++ src/constants/Assets.tsx
@@ -0,0 +1,19 @@
+export const FirstCategoryEmojis = {
+  restaurant: require('../../assets/images/emojis/restaurants.png'),
+  cafe: require('../../assets/images/emojis/cafe.png'),
+  class: require('../../assets/images/emojis/gift.png'),
+  curation: require('../../assets/images/emojis/curation.png'),
+  pub: require('../../assets/images/emojis/pub.png'),
+  gym: require('../../assets/images/emojis/gym.png'),
+  kids: require('../../assets/images/emojis/kids.png'),
+  beauty: require('../../assets/images/emojis/beauty.png'),
+  flowershop: require('../../assets/images/emojis/flowershop.png'),
+  education: require('../../assets/images/emojis/education.png'),
+  studio: require('../../assets/images/emojis/studio.png'),
+  pets: require('../../assets/images/emojis/pets.png'),
+};
+
+export const Logo = {
+  bw: require('../../assets/images/BI/LogoGrey.png'),
+  color: require('../../assets/images/BI/logo.png'),
+};
