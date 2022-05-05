f078a8104d8a48fa5ad49e67183205086cdf2701
르네상스맨 <whdtmek@gmail.com>
2022-05-05 20:23:34 +0900

    [PlaceLobby] 리팩터링 : Header 컴포넌트화

--- src/screens/Place/PlaceLobby/PlaceLobby.tsx
+++ src/screens/Place/PlaceLobby/PlaceLobby.tsx
@@ -2,12 +2,8 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {View, Text} from 'react-native';
 import React from 'react';
 
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {PlaceTabStackParamList} from '../../../../navigation/PlaceTabStackParams';
-import {useNavigation} from '@react-navigation/native';
-
 import {ThemeProvider} from 'styled-components';
-import styled, {DefaultTheme} from 'styled-components/native';
+import styled from 'styled-components/native';
 import Theme from '../../../styles/Theme';
 
 import FastImage from 'react-native-fast-image';
@@ -18,85 +14,80 @@ import {Logo, FirstCategoryEmojis} from '../../../constants/Assets';
 
 import {ButtonLarge} from './components/PlaceLobbyButtons';
 
-interface StyledProps {
-  theme: DefaultTheme;
-}
-
 const ContentScreen = styled.ScrollView`
   flex: 1;
 `;
 
-const PlaceLobbyHeader = styled.View`
+const HeaderContainer = styled.View`
   display: flex;
   justify-content: center;
   border-bottom: solid, 1px;
 `;
 
-type placeLobbyProp = NativeStackNavigationProp<
-  PlaceTabStackParamList,
-  'PlaceLobby'
->;
+function PlaceLobbyHeader() {
+  return (
+    <HeaderContainer
+      style={{
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 6},
+        shadowOpacity: 0.15,
+        shadowRadius: 3,
+        elevation: 5,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        backgroundColor: 'white',
+      }}>
+      <View
+        style={{
+          padding: 17,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{flexDirection: 'row', height: '100%'}}>
+          <View
+            style={{
+              width: 26,
+              height: 28,
+              marginLeft: 6,
+              marginRight: 16,
+            }}>
+            <FastImage
+              style={{width: '100%', height: '100%'}}
+              source={Logo.color}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              height: '100%',
+              // paddingTop: 5,
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+              }}>
+              강원도 원주시 {'>'}
+            </Text>
+          </View>
+        </View>
+        <View>
+          <IonIcons name="notifications-outline" size={25} />
+        </View>
+      </View>
+    </HeaderContainer>
+  );
+}
 
 interface Props {}
 const PlaceLobby = ({}: Props) => {
-  const navigation = useNavigation<placeLobbyProp>();
-
   return (
     <ThemeProvider theme={Theme}>
       <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
         <View style={{flex: 1}}>
-          <PlaceLobbyHeader
-            style={{
-              shadowColor: '#000',
-              shadowOffset: {width: 0, height: 6},
-              shadowOpacity: 0.15,
-              shadowRadius: 3,
-              elevation: 5,
-              borderBottomLeftRadius: 10,
-              borderBottomRightRadius: 10,
-              backgroundColor: 'white',
-            }}>
-            <View
-              style={{
-                padding: 17,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}>
-              <View style={{flexDirection: 'row', height: '100%'}}>
-                <View
-                  style={{
-                    width: 26,
-                    height: 28,
-                    marginLeft: 6,
-                    marginRight: 16,
-                  }}>
-                  <FastImage
-                    style={{width: '100%', height: '100%'}}
-                    source={Logo.color}
-                  />
-                </View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    height: '100%',
-                    // paddingTop: 5,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 18,
-                    }}>
-                    강원도 원주시 {'>'}
-                  </Text>
-                </View>
-              </View>
-              <View>
-                <IonIcons name="notifications-outline" size={25} />
-              </View>
-            </View>
-          </PlaceLobbyHeader>
+          <PlaceLobbyHeader />
           <ContentScreen>
             <View
               style={{
