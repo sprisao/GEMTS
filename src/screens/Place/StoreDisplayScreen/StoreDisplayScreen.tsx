c409bc0d1d8660aadd8c2c93d37db7237eff110a
르네상스맨 <whdtmek@gmail.com>
2022-05-07 14:37:52 +0900

    [StoreDisplayScreen] 네비게이션 기능 추가

--- src/screens/Place/StoreDisplayScreen/StoreDisplayScreen.tsx
+++ src/screens/Place/StoreDisplayScreen/StoreDisplayScreen.tsx
@@ -1,12 +1,35 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RouteProp} from '@react-navigation/native';
+import {PlaceTabStackParamList} from '../../../../navigation/PlaceTabStackParams';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
+type StoreDisplayScreenNavigationProp = NativeStackNavigationProp<
+  PlaceTabStackParamList,
+  'PlaceSecondLobby'
+>;
+
+type StoreDisplayScreenRouteProp = RouteProp<
+  PlaceTabStackParamList,
+  'PlaceSecondLobby'
+>;
+
 type Props = {};
 
 const StoreDisplayScreen = (props: Props) => {
+  const route = useRoute<StoreDisplayScreenRouteProp>();
+  const navigation = useNavigation<StoreDisplayScreenNavigationProp>();
+
+  const _firstCategoryId = route.params.firstCategoryId;
+  const _initialFocus = route.params.initialFocus;
+  console.log(_initialFocus);
+
   return (
     <View>
-      <Text>StoreDisplayScreen</Text>
+      <Text>{_firstCategoryId}</Text>
+      <Text>{_initialFocus}</Text>
     </View>
   );
 };
