cb5d36f2102f21974c63a88f27eaabbcdfb6c000
르네상스맨 <whdtmek@gmail.com>
2022-05-12 10:21:30 +0900

    [Firestore] 로딩속도, 화면 업데이트 , 좋아요 적용 : Pagination + onReachedEnd + geMore 등으로 퍼포먼스 및 데이터 불러오는 효율 증가 - 1

--- src/screens/Place/StoreDisplayScreen/StoreDisplayScreen.tsx
+++ src/screens/Place/StoreDisplayScreen/StoreDisplayScreen.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {useState, useEffect, useRef, useMemo} from 'react';
-import {FlatList, View, Text, TouchableOpacity} from 'react-native';
+import {
+  FlatList,
+  View,
+  Text,
+  TouchableOpacity,
+  RefreshControl,
+} from 'react-native';
 
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
@@ -32,12 +38,23 @@ const StoreDisplayScreen = ({}: Props) => {
   const _secondCategories = route.params.secondCategories;
   const _initialFocus = route.params.initialFocus;
 
-  const [stores, setStores] = useState([]);
+  // const [stores, setStores] = useState([]);
   const [currentFocus, setCurrentFocus] = useState(_initialFocus);
 
   const tabRef = useRef();
 
   const thisUid = auth().currentUser.uid;
+
+  let onEndReachedCalledDuringMomentum = false;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isMoreLoading, setIsMoreLoading] = useState(false);
+  const [lastDoc, setLastDoc] = useState(null);
+  const [stores, setStores] = useState([]);
+
+  const storesRef = firestore()
+    .collection('stores_new')
+    .where('firstCategoryId', 'array-contains', _firstCategoryId);
 
   /////////////////////// Data 가져오기 1차 시도
   // useEffect(() => {
@@ -62,12 +79,68 @@ const StoreDisplayScreen = ({}: Props) => {
   //   return () => getStores();
   // }, []);
 
-  // function onCategorySelect(e) {
-  //   setCurrentFocus(e);
-  // }
-
   /////////////////// 2차 시도
 
+  useEffect(() => {
+    getStores();
+  }, []);
+
+  const getStores = async () => {
+    setIsLoading(true);
+
+    const snapshot = await storesRef.limit(50).get();
+
+    if (!snapshot.empty) {
+      let _stores = [];
+
+      setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
+
+      for (let i = 0; i < snapshot.docs.length; i++) {
+        _stores.push(snapshot.docs[i].data());
+      }
+
+      setStores(_stores);
+    } else {
+      setLastDoc(null);
+    }
+
+    setIsLoading(false);
+  };
+
+  const getMore = async () => {
+    if (lastDoc) {
+      setIsMoreLoading(true);
+
+      setTimeout(async () => {
+        let snapshot = await storesRef.startAfter(lastDoc).limit(100).get();
+
+        if (!snapshot.empty) {
+          let newRestaurants = stores;
+
+          setLastDoc(snapshot.docs[snapshot.docs.length - 1]);
+
+          for (let i = 0; i < snapshot.docs.length; i++) {
+            newRestaurants.push(snapshot.docs[i].data());
+          }
+
+          setStores(newRestaurants);
+          if (snapshot.docs.length < 3) {
+            setLastDoc(null);
+          }
+        } else {
+          setLastDoc(null);
+        }
+
+        setIsMoreLoading(false);
+      }, 1000);
+    }
+
+    onEndReachedCalledDuringMomentum = true;
+  };
+
+  function onCategorySelect(e) {
+    setCurrentFocus(e);
+  }
   function allTab() {
     return (
       <TouchableOpacity
@@ -84,6 +157,12 @@ const StoreDisplayScreen = ({}: Props) => {
       </TouchableOpacity>
     );
   }
+
+  const onRefresh = () => {
+    setTimeout(() => {
+      getStores();
+    }, 1000);
+  };
 
   function tabs({item, index}) {
     return (
@@ -152,16 +231,32 @@ const StoreDisplayScreen = ({}: Props) => {
         })}
         renderItem={momoizedTabs}
       />
+
       <FlatList
         data={stores}
         windowSize={10}
         numColumns={2}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={() => onRefresh()}
+          />
+        }
+        onEndReachedThreshold={0.7}
+        onMomentumScrollBegin={() => {
+          onEndReachedCalledDuringMomentum = false;
+        }}
+        onEndReached={() => {
+          if (!onEndReachedCalledDuringMomentum && !isMoreLoading) {
+            getMore();
+          }
+        }}
         columnWrapperStyle={{
           justifyContent: 'space-between',
           marginBottom: 10,
           paddingHorizontal: 8,
         }}
-        initialNumToRender={10}
+        initialNumToRender={20}
         maxToRenderPerBatch={30}
         renderItem={({item}) => (
           // 여기서 나중에 높이 조절해야함 -> 스타일링 끝나고 마무리는 fixed height 설정 -> 렌더링 속도 업!
