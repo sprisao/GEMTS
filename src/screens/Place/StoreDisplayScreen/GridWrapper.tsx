db7e3913cbf92c3b330a6aafa2ed2d244d398e3f
르네상스맨 <whdtmek@gmail.com>
2022-05-23 17:53:08 +0900

    [GridWrapper] 최신 코드에 맞게 수정

--- src/screens/Place/StoreDisplayScreen/GridWrapper.tsx
+++ src/screens/Place/StoreDisplayScreen/GridWrapper.tsx
@@ -1,8 +1,12 @@
 import React, {useMemo} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, RefreshControl, StyleSheet, View} from 'react-native';
+import {useGlobalContext} from '../../../Contexts/placeContext.';
+import {PlaceContext} from '../../../Contexts/placeContext.';
 import GridTile from './GridTile';
 
 const GridWrapper = props => {
+  const {isLoading, getMore, onEndReachedCalledDuringMomentum, isMoreLoading} =
+    useGlobalContext();
   let spotFilter = '';
   if (props.isSpot) {
     spotFilter = 'SpotDetails';
@@ -38,13 +42,28 @@ const GridWrapper = props => {
     <View style={styles.screen}>
       <FlatList
         data={props.data}
-        keyExtractor={(item, index) => item.id}
+        keyExtractor={item => item.name}
         renderItem={memoizedValue}
         style={{flex: 1}}
         columnWrapperStyle={{justifyContent: 'space-between'}}
         numColumns={2}
         windowSize={2}
         bounces={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={() => onRefresh()}
+          />
+        }
+        onEndReachedThreshold={0.7}
+        onMomentumScrollBegin={() => {
+          onEndReachedCalledDuringMomentum = false;
+        }}
+        onEndReached={() => {
+          if (!onEndReachedCalledDuringMomentum && !isMoreLoading) {
+            getMore(props.firstCategory);
+          }
+        }}
         disableVirtualization={false} //비정상적인 스크롤 동작을 방지하려고
       />
     </View>
