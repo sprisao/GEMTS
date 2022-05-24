d2066940ab31d160d17a8d61ba6af154e551502b
르네상스맨 <whdtmek@gmail.com>
2022-05-24 15:33:50 +0900

    [DetailScreen] StoreDetail 및 관련 컴포넌트 추가

---
+++ src/screens/Place/StoreDetailScreen/DetailsCuration.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, View, Dimensions, ScrollView } from "react-native";
+
+import FastImage from "react-native-fast-image";
+
+const DetailsCuration = props => {
+  const storeData = props.route.params.storeData;
+  const deviceWidth = Dimensions.get("window").width;
+  const curationWidth = storeData.curation[0].width;
+  const curationHeight = storeData.curation[0].height;
+  const theScale = curationWidth / deviceWidth;
+  const scaledHeight = curationHeight / theScale;
+
+  return (<ScrollView style={styles.detailsCuration} bounces={false}>
+    <View style={styles.curationContainer}>
+      <FastImage
+        source={{ uri: storeData.curation[0].url }}
+        style={{
+          width: Dimensions.get("window").width, height: scaledHeight,
+        }}
+        resizeMode={FastImage.resizeMode.cover}
+      />
+    </View>
+  </ScrollView>);
+};
+
+export default DetailsCuration;
+
+const styles = StyleSheet.create({
+  detailsCurationWrapper: { flex: 1, width: "100%", paddingHorizontal: 10 }, detailsCurationHeader: {
+    fontFamily: "SD-B", fontSize: 22, marginVertical: 7, letterSpacing: -1,
+  }, curationContainer: {
+    width: "100%",
+  },
+});
