52850f327cd61abf5c87c92bbca284290597246e
르네상스맨 <whdtmek@gmail.com>
2022-05-25 16:04:01 +0900

    [StyledComponents] ThemeProvider 위치 App.tsx 로 이동

--- App.tsx
+++ App.tsx
@@ -7,47 +7,53 @@ import CommunityTabStack from './navigation/CommunityTabStack';
 import ChatTabStack from './navigation/ChatTabStack';
 import AccountTabStack from './navigation/AccountTabStack';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Theme from './src/styles/Theme';
+import {ThemeProvider} from 'styled-components';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <PlaceProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              let iconName;
-              if (route.name === 'PlaceTab') {
-                iconName = focused ? 'search' : 'search-outline';
-              } else if (route.name === 'ChatTab') {
-                iconName = focused
-                  ? 'ios-chatbubble-ellipses-sharp'
-                  : 'ios-chatbubble-ellipses-outline';
-              } else if (route.name === 'CommunityTab') {
-                iconName = focused ? 'flash' : 'flash-outline';
-              } else if (route.name === 'AccountTab') {
-                iconName = focused ? 'person-circle' : 'person-circle-outline';
-              }
-              // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: 'black',
-            tabBarInactiveTintColor: 'gray',
-            headerShown: false,
-            tabBarShowLabel: false,
-          })}>
-          <Tab.Screen name="PlaceTab" component={PlaceTabStack} />
-          <Tab.Screen name="CommunityTab" component={CommunityTabStack} />
-          <Tab.Screen
-            name="ChatTab"
-            component={ChatTabStack}
-            options={{tabBarBadge: 3}}
-          />
-          <Tab.Screen name="AccountTab" component={AccountTabStack} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </PlaceProvider>
+    <ThemeProvider theme={Theme}>
+      <PlaceProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                if (route.name === 'PlaceTab') {
+                  iconName = focused ? 'search' : 'search-outline';
+                } else if (route.name === 'ChatTab') {
+                  iconName = focused
+                    ? 'ios-chatbubble-ellipses-sharp'
+                    : 'ios-chatbubble-ellipses-outline';
+                } else if (route.name === 'CommunityTab') {
+                  iconName = focused ? 'flash' : 'flash-outline';
+                } else if (route.name === 'AccountTab') {
+                  iconName = focused
+                    ? 'person-circle'
+                    : 'person-circle-outline';
+                }
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: 'black',
+              tabBarInactiveTintColor: 'gray',
+              headerShown: false,
+              tabBarShowLabel: false,
+            })}>
+            <Tab.Screen name="PlaceTab" component={PlaceTabStack} />
+            <Tab.Screen name="CommunityTab" component={CommunityTabStack} />
+            <Tab.Screen
+              name="ChatTab"
+              component={ChatTabStack}
+              options={{tabBarBadge: 3}}
+            />
+            <Tab.Screen name="AccountTab" component={AccountTabStack} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PlaceProvider>
+    </ThemeProvider>
   );
 };
 export default App;
--- src/screens/Place/PlaceLobby/PlaceLobby.tsx
+++ src/screens/Place/PlaceLobby/PlaceLobby.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import {ThemeProvider} from 'styled-components';
 import styled from 'styled-components/native';
-import Theme from '../../../styles/Theme';
 
 import {FirstCategoryEmojis} from '../../../constants/Assets';
 
@@ -12,6 +10,7 @@ import {
   ButtonMedium,
   ButtonSmall,
 } from './components/PlaceLobbyButtons';
+
 import Banner from './components/Banner';
 
 const SafeAreaView = styled.SafeAreaView`
@@ -34,98 +33,97 @@ const ButtonWrapper = styled.View`
 `;
 
 interface Props {}
+
 const PlaceLobby = ({}: Props) => {
   return (
-    <ThemeProvider theme={Theme}>
-      <SafeAreaView>
-        <PlaceLobbyHeader />
-        <ScrollView>
-          <ButtonWrapper>
-            <ButtonLarge
-              name={'맛집'}
-              message={'검색없이 한번에\n바로찾는 원주맛집'}
-              id={'recxEYsUuSaVk3ge2'}
-              emoji={FirstCategoryEmojis.restaurant}
-            />
-            <ButtonLarge
-              name={'카페'}
-              message={'원주의\n모든 카페를 한 눈에!'}
-              id={'rec1nohULLWQVqXZD'}
-              emoji={FirstCategoryEmojis.cafe}
-            />
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <ButtonMedium
-              name={'젬 클래스'}
-              id={'service'}
-              message={'우리동네 재능마켓\n다양한 클래스'}
-              emoji={FirstCategoryEmojis.class}
-            />
-            <ButtonMedium
-              name={'젬 큐레이션'}
-              id={'curation'}
-              message={'젬이 소개하는\n보석같은 장소들'}
-              emoji={FirstCategoryEmojis.curation}
-            />
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <Banner />
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <ButtonSmall
-              id="rectbHPCk3LZzfu6w"
-              name="호프주점"
-              emoji={FirstCategoryEmojis.pub}
-              message={''}
-            />
-            <ButtonSmall
-              id="recTQQfv194uE5O1K"
-              name="운동헬스"
-              emoji={FirstCategoryEmojis.gym}
-              message={''}
-            />
-            <ButtonSmall
-              id="recse3cQXji2BWlNu"
-              name="미용뷰티"
-              emoji={FirstCategoryEmojis.beauty}
-              message={''}
-            />
-            <ButtonSmall
-              id="recqOzD4oznYyHeXS"
-              name="사진"
-              emoji={FirstCategoryEmojis.studio}
-              message={''}
-            />
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <ButtonSmall
-              id="rec9dQcEElsgcAsD7"
-              name="반려동물"
-              emoji={FirstCategoryEmojis.pets}
-              message={''}
-            />
-            <ButtonSmall
-              id="recoF9HKKfA3ZSxSP"
-              name="꽃"
-              emoji={FirstCategoryEmojis.flowershop}
-              message={''}
-            />
-            <ButtonSmall
-              id="recXj7vVqBBJttGd8"
-              name="키즈"
-              emoji={FirstCategoryEmojis.kids}
-              message={''}
-            />
-            <ButtonSmall
-              id="recz1ftDhPT8VADjf"
-              name="교육학원"
-              emoji={FirstCategoryEmojis.education}
-              message={''}
-            />
-          </ButtonWrapper>
-        </ScrollView>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaView>
+      <PlaceLobbyHeader />
+      <ScrollView>
+        <ButtonWrapper>
+          <ButtonLarge
+            name={'맛집'}
+            message={'검색없이 한번에\n바로찾는 원주맛집'}
+            id={'recxEYsUuSaVk3ge2'}
+            emoji={FirstCategoryEmojis.restaurant}
+          />
+          <ButtonLarge
+            name={'카페'}
+            message={'원주의\n모든 카페를 한 눈에!'}
+            id={'rec1nohULLWQVqXZD'}
+            emoji={FirstCategoryEmojis.cafe}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <ButtonMedium
+            name={'젬 클래스'}
+            id={'service'}
+            message={'우리동네 재능마켓\n다양한 클래스'}
+            emoji={FirstCategoryEmojis.class}
+          />
+          <ButtonMedium
+            name={'젬 큐레이션'}
+            id={'curation'}
+            message={'젬이 소개하는\n보석같은 장소들'}
+            emoji={FirstCategoryEmojis.curation}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Banner />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <ButtonSmall
+            id="rectbHPCk3LZzfu6w"
+            name="호프주점"
+            emoji={FirstCategoryEmojis.pub}
+            message={''}
+          />
+          <ButtonSmall
+            id="recTQQfv194uE5O1K"
+            name="운동헬스"
+            emoji={FirstCategoryEmojis.gym}
+            message={''}
+          />
+          <ButtonSmall
+            id="recse3cQXji2BWlNu"
+            name="미용뷰티"
+            emoji={FirstCategoryEmojis.beauty}
+            message={''}
+          />
+          <ButtonSmall
+            id="recqOzD4oznYyHeXS"
+            name="사진"
+            emoji={FirstCategoryEmojis.studio}
+            message={''}
+          />
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <ButtonSmall
+            id="rec9dQcEElsgcAsD7"
+            name="반려동물"
+            emoji={FirstCategoryEmojis.pets}
+            message={''}
+          />
+          <ButtonSmall
+            id="recoF9HKKfA3ZSxSP"
+            name="꽃"
+            emoji={FirstCategoryEmojis.flowershop}
+            message={''}
+          />
+          <ButtonSmall
+            id="recXj7vVqBBJttGd8"
+            name="키즈"
+            emoji={FirstCategoryEmojis.kids}
+            message={''}
+          />
+          <ButtonSmall
+            id="recz1ftDhPT8VADjf"
+            name="교육학원"
+            emoji={FirstCategoryEmojis.education}
+            message={''}
+          />
+        </ButtonWrapper>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
